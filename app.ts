600dce556042829e70bfe4f1843c0ad18bac3c71
AJ Collins <16407457+ajc133@users.noreply.github.com>
2023-01-11 22:36:54 -0800

    initial commit with code

---
+++ app.ts
@@ -0,0 +1,5 @@
+let message: string = 'Hello, World!';
+let input = document.createElement('input');
+input.placeholder = message;
+// add the heading the document
+document.body.appendChild(input);
