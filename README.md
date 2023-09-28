ee814ef7ce97da3db0538bc6cdc72443c7e3c8cd
AJ Collins <16407457+ajc133@users.noreply.github.com>
2023-09-27 22:15:19 -0700

    chore: Create TODO list

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Calendar Proxy
 
 Proxy that changes calendar events in a pre-defined way
+
+# TODO
+
+- rename repo to calendarProxy
+
+## DB
+
+- Create table if not exists at server start
+- Write tests
+
+## Server
+
+- Move to basic net/http server
+- Set trusted proxies https://pkg.go.dev/github.com/gin-gonic/gin#Engine.SetHTMLTemplate
+- Better error message when input is malformed
+- Write tests
+
+## App
+
+- Fetch calendar
+- Parse calendar
+- Write tests
--- main.go
+++ main.go
@@ -9,45 +9,50 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	router := gin.Default()
-
-	// TODO: create table at server start
-	router.StaticFile("/", "./static/index.html")
-	router.GET("/:id", getCalendarByID)
-	router.POST("/", createCalendar)
-	router.Run("localhost:8080")
-}
-
 type CalendarParams struct {
 	Url                string `form:"url" json:"url" binding:"required"`
 	ReplacementSummary string `form:"replacementSummary" json:"replacementSummary" binding:"required"`
 }
 
-func createCalendar(c *gin.Context) {
+func main() {
+	CreateDB()
+	router := gin.Default()
+	router.StaticFile("/", "./static/index.html")
+	router.GET("/:id", GetCalendarByID)
+	router.POST("/", CreateCalendar)
+	router.Run("localhost:8080")
+}
+
+func CreateCalendar(c *gin.Context) {
 	json := CalendarParams{}
-	err := c.ShouldBind(&json)
+	err := c.Bind(&json)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
 	}
 
-	// TODO: validate url is a url
-
-	writeRecord(json)
+	WriteRecord(json)
 	c.JSON(http.StatusOK, gin.H{"status": "successfully added entry"})
 }
 
-func getCalendarByID(c *gin.Context) {
+func GetCalendarByID(c *gin.Context) {
 	id := c.Param("id")
-	url, err := readRecord(id)
+	url, err := ReadRecord(id)
 	if err != nil {
 		log.Fatal(err)
 	}
 	c.JSON(http.StatusOK, gin.H{"id": id, "url": url})
 }
+func CreateDB() error {
+	db, err := sql.Open("sqlite3", "./calendars.db")
+	stmt := "CREATE TABLE IF NOT EXISTS calendars(id INTEGER PRIMARY KEY, url TEXT, replacementSummary TEXT);"
+	_, err = db.Exec(stmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
 
-func readRecord(id string) (string, error) {
+func ReadRecord(id string) (string, error) {
 	db, err := sql.Open("sqlite3", "./calendars.db")
 	if err != nil {
 		return "", err
@@ -58,7 +63,7 @@ func readRecord(id string) (string, error) {
 		return "", err
 	}
 
-	stmt, err := db.Prepare("select url from calendars where id =?")
+	stmt, err := db.Prepare("select url from calendars where id = ?")
 	if err != nil {
 		return "", err
 	}
@@ -72,23 +77,17 @@ func readRecord(id string) (string, error) {
 
 }
 
-func writeRecord(params CalendarParams) {
+func WriteRecord(params CalendarParams) {
 	db, err := sql.Open("sqlite3", "./calendars.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	stmt := "CREATE TABLE IF NOT EXISTS calendars(id INTEGER PRIMARY KEY, url TEXT, replacementSummary TEXT);"
-	_, err = db.Exec(stmt)
+	stmt := "INSERT INTO calendars(url, replacementSummary) VALUES(?, ?);"
+	result, err := db.Exec(stmt, params.Url, params.ReplacementSummary)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	stmt = "INSERT INTO calendars(url, replacementSummary) VALUES(?, ?);"
-	_, err = db.Exec(stmt, params.Url, params.ReplacementSummary)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// TODO: return id of created record
+	log.Println(result.LastInsertId())
 }
